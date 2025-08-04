5af7495eac4500e0587c2f356f1b5ee1a653223f
RowanKinross <rowankinross@yahoo.co.uk>
2025-08-04 15:03:53 +0100

    should have checked it was working in local first lol

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import dayjs from 'dayjs';
 import './App.css';
-import DoughCalculator from './src/components/doughCalculator';
+import DoughCalculator from './components/doughCalculator';
 
 
 function App() {
